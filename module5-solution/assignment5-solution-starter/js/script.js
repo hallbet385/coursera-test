c5fa11fc5024d3b9ef7e258e67db2fb6bb0a3b90
Tania Sands <tsands@nycm.com>
2023-10-05 10:43:36 -0400

    upload 13

--- module5-solution/assignment5-solution-starter/js/script.js
+++ module5-solution/assignment5-solution-starter/js/script.js
@@ -106,7 +106,7 @@ function buildAndShowHomeHTML (categories) {
       var chosenCategoryShortName = 
 		chooseRandomCategory (categories); 
 		
-		console.log(chosenCategoryShortName);
+		//console.log(chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -124,7 +124,8 @@ function buildAndShowHomeHTML (categories) {
 	  //var randomCategoryShortName = chosenCategoryShortName.name;
 	  homeHtmlToInsertIntoMainPage = 
 		insertProperty(homeHtmlToInsertIntoMainPage, "randomCategoryShortName", chosenCategoryShortName.short_name);
-
+		
+		console.log(homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
@@ -145,7 +146,6 @@ function chooseRandomCategory (categories) {
 
   // return category object with that randomArrayIndex
   return categories[randomArrayIndex];
-  console.log (categories[randomArrayIndex]);
 }
 
 
