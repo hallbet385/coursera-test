98d6049f3fc6daccd9e8e1447be899f6e11d1472
Tania Sands <tsands@nycm.com>
2023-10-05 10:25:42 -0400

    upload 11

--- module5-solution/assignment5-solution-starter/js/script.js
+++ module5-solution/assignment5-solution-starter/js/script.js
@@ -105,7 +105,6 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       var chosenCategoryShortName = 
 		chooseRandomCategory (categories); 
-			console.log (chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -122,7 +121,7 @@ function buildAndShowHomeHTML (categories) {
       //var homeHtmlToInsertIntoMainPage = homeHtmlUrl;  
 	  //var randomCategoryShortName = chosenCategoryShortName.name;
 	  var homeHtmlToInsertIntoMainPage = 
-		insertProperty(homeHtmlUrl, "randomCategoryShortName", chosenCategoryShortName.short_name);
+		insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName.short_name);
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
